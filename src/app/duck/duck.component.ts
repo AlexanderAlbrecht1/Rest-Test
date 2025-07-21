8d43753cf608ff24021e9f9a7d3a61d4f4f0cb30
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-07-21 21:11:38 +0200

    back button quack

--- src/app/duck/duck.component.ts
+++ src/app/duck/duck.component.ts
@@ -1,10 +1,11 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-duck',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './duck.component.html',
   styleUrl: './duck.component.scss'
 })
