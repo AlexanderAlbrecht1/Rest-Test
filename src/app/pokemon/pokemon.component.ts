53989c2c058ba91774f7cc1d6073fc783866f5c1
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-07-21 21:26:15 +0200

    start pkmn

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.scss'
 })
 export class PokemonComponent {
+
+  pokeNumber : number = 161;
+  baseUrl:string = 'https://pokeapi.co/api/v2/pokemon/'
+  currentPokemon : string = '';
+
+  loadPokemon() {
+    console.error('hier pssiert gerade nix!');
+
+  }
 
 }
